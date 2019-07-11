caf6aa02a02794bba3720710c9b692bea7e38144
karan.maru <karan.maru@plasticmobile.com>
2019-07-11 15:34:27 -0400

    provide app center configuration

--- app/src/main/java/com/plastic/testappcenter/MainActivity.kt
+++ app/src/main/java/com/plastic/testappcenter/MainActivity.kt
@@ -2,11 +2,19 @@ package com.plastic.testappcenter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "47b43a63-ef4b-421b-a5fc-d6a83233d0cd",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
